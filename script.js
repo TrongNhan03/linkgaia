1556109506e2a5c0b5a3620fd7086ff0fe49d94e
TrongNhan <trongnhan0703it@gmail.com>
2024-09-05 08:26:15 +0700

    update permission

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
-let btn=document.getElementsByTagName('button')[0]
+let btn=document.getElementById('btnMore')
+let btnconfirm=document.getElementById('confirm')
 let moretab=document.getElementById('more')
+let popular=document.getElementById('popular')
+let inputpass=document.getElementsByTagName('input')[0]
+
+inputpass.focus()
+btnconfirm.onclick=()=>{
+    if(inputpass.value == 'kythuat123') {
+        document.getElementById('permission').style.display='none'
+        btn.style.display='block';
+        popular.style.display='flex';
+        document.getElementsByTagName('body')[0].addEventListener('keydown',(e)=>{
+            if(e.key=='1') {
+                document.querySelectorAll('#popular>a')[0].click();
+            }
+            if(e.key=='2') {
+                document.querySelectorAll('#popular>a')[1].click();
+            }
+        })
+    }
+    else {
+        alert('Sai mật khẩu')
+    }
+}
+inputpass.addEventListener('keydown',(e)=>{
+    if(e.key=='Enter') {
+        btnconfirm.click();
+    }
+})
+
 btn.innerText='More'
 btn.onclick=()=> {
     if(btn.innerText=='More') {
